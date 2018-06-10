4470cf73a5062ba3b4ef52e9c0d263926a5ad87b
Edon Gashi <edon.gashi@uni-pr.edu>
2018-06-10 19:29:25 +0200

    Updated authserver project

--- src/AuthServer/Startup.cs
+++ src/AuthServer/Startup.cs
@@ -17,12 +17,15 @@ namespace AuthServer
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
         }
 
         public IConfiguration Configuration { get; }
+
+        public IHostingEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
