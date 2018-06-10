631ebc27041ff68b007c2b7040119a12b41174a8
Edon Gashi <edon.gashi@uni-pr.edu>
2018-06-10 19:31:21 +0200

    Updated Program.cs to initialize assembly

--- src/AuthServer/Program.cs
+++ src/AuthServer/Program.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Utils.Initialization;
 
 namespace AuthServer
 {
@@ -14,11 +9,14 @@ namespace AuthServer
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            host.Services.InitializeAssembly(Assembly.GetExecutingAssembly()).Wait();
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            WebHost
+                .CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
     }
 }
