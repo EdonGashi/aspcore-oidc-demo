b6de0afbdac9f26979c0002855cbba3b70944cbb
Edon Gashi <edon.gashi@uni-pr.edu>
2018-06-10 19:32:11 +0200

    Added constants

---
+++ src/AuthServer/AppConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace AuthServer
+{
+    public static class AppConstants
+    {
+        public static class Scopes
+        {
+
+        }
+
+        public static class Roles
+        {
+            public const string Admin = "admin";
+        }
+    }
+}
